442291a6a704c5f2ebf63bbd5195d49c58141794
Farid <farid.hammouten@gmail.com>
2025-07-01 01:34:03 +0200

    set basic express server

---
+++ apps/erp-api/src/index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import morgan from "morgan"
+import dotenv from "dotenv"
+import routes from "./routes/index.js"
+
+dotenv.config()
+
+const app = express.app()
+const port = process.env.PORT || 3000;
+
+app.use(morgan('dev'))
+app.use(express.json())
+
+app.use("/api", routes)
+
+app.get("/", (_, res) => res.send("ERP API is fucking working 🖕"))
+
+app.listen(port, ()=> console.log(`ERP-API up on http://localhost:${port}`))
